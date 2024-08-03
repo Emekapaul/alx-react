43a8387f5ab14dc5c446c88da6295673f65bd323
Paul Nnaemeka <emekapaul302@gmail.com>
2024-08-03 06:12:56 +0100

    Solve task 11 0x01-react_intro

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import holberton_logo from "../assets/holberton-logo.jpg";
+import { getFullYear, getFooterCopy } from '../utils/utils';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        < input type="email" id="email" name="email" />
+
+        <label htmlFor="password">password:</label>
+        <input type="password" id="password" name="password" />
+
+        <button>OK</button>
+      </div>
+
+      <div className='App-footer'>
+        <p>{ getFooterCopy(true)} {getFullYear()}</p>
+      </div>
+    </>
+  );
+}
