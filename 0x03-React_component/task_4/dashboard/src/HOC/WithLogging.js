1e69fe98a20db69a48304c4ea628995d3835dfb7
Paul Nnaemeka <emekapaul302@gmail.com>
2024-08-10 08:09:46 +0100

    Solve task 8 0x03-React_component

--- 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -4,18 +4,22 @@ export default function WithLogging(WrappedComponent) {
   class WithLogging extends Component {
     componentDidMount() {
       const componentName = (WrappedComponent.displayName || WrappedComponent.name || "Component");
-      console.log(`${componentName} is mounted`);
+      console.log(`Component ${componentName} is mounted`);
     }
 
     componentWillUnmount() {
       const componentName = (WrappedComponent.displayName || WrappedComponent.name || "Component");
-      console.log(`${componentName} is going to unmount`);
+      console.log(`Component ${componentName} is going to unmount`);
     }
 
     render() {
+      // Render the wrapped component and pass down any props
       return <WrappedComponent {...this.props} />
     }
   }
 
+  // Set the displayName for the HOC
   WithLogging.displayName = `WithLogging${WrappedComponent.displayName || WrappedComponent.name || "Component"}`;
+
+  return WithLogging;
 }
