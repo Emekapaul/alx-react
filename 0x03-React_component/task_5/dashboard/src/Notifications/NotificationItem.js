71fded117fc27f9b5f537e8c2101b0f3913c9c00
Paul Nnaemeka <emekapaul302@gmail.com>
2024-08-10 08:40:19 +0100

    Solve task 9 0x03-React_component

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,23 @@
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+
+function NotificationItem({ id, type = 'default', html, value, markAsRead = () => { } }) {
+  return (
+    <>
+      {type && value ? <li onClick={() => markAsRead(id)} data-notification-type={type}>{value}</li> : null}
+      {html ? <li onClick={() => markAsRead(id)} dangerouslySetInnerHTML={html} className="dangerous-notification"></li> : null}
+    </>
+  );
+}
+
+NotificationItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string
+  }),
+  type: PropTypes.string,
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+};
+
+export default memo(NotificationItem);
