84b9e8d4da3ee0ca8cea17377d2885c8f9d4a55a
Paul Nnaemeka <emekapaul302@gmail.com>
2024-08-28 19:10:52 +0100

    Solve task 7 0x07-react_redux_action_creator

---
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import NotificationItem from "./NotificationItem";
+import { StyleSheetTestUtils } from 'aphrodite';
+
+beforeAll(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterAll(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe('NotificationItem Component', () => {
+  test('renders without crashing', () => {
+    const { container } = render(<NotificationItem />);
+    expect(container).toBeInTheDocument();
+  });
+
+  test('renders the correct html when "type" & "value" props is passed', () => {
+    const { container } = render(<NotificationItem type="default" value="Test notification" />);
+    const dataAttr = container.querySelector('li[data-notification-type]');
+    expect(dataAttr).toBeInTheDocument()
+    expect(dataAttr).toHaveAttribute('data-notification-type', 'default');
+    screen.getByText('Test notification', { selector: 'li[data-notification-type]' });
+  });
+
+  test('renders the correct html when "html" props is passed', () => {
+    const { container } = render(<NotificationItem html={{ __html: '<u>test</u>' }} />);
+    const htmlElement = container.querySelector('u');
+    expect(htmlElement).toBeInTheDocument();
+    screen.getByText('test', { selector: 'u' })
+  });
+
+  test('calls markAsRead with the correct ID when clicked', () => {
+    const markAsReadSpy = jest.fn();
+    const testId = 1;
+    render(
+      <NotificationItem
+        markAsRead={markAsReadSpy}
+        id={testId}
+        type='defauly'
+        value='Test Notification'
+      />
+    );
+    const notificationItem = screen.getByText('Test Notification');
+    fireEvent.click(notificationItem);
+
+    expect(markAsReadSpy).toHaveBeenCalledWith(testId);
+  })
+})
---
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,9 @@
+// action types:
+export const MARK_AS_READ = 'MARK_AS_READ';
+export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
+
+// filter states
+export const NotificationTypeFilters = {
+  DEFAULT: 'DEFAULT',
+  URGENT: 'URGENT'
+};
